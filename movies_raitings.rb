154fd35bdbcd08c7799d7258259de86afc693794
frolin <fralunia@gmail.com>
2015-11-09 20:36:50 +0300

    clean lesson_3 method

--- movies_raitings.rb
+++ movies_raitings.rb
@@ -33,30 +33,36 @@ end
 
 
 def movies_by_duration(movies_list)
-   movies_list.sort_by { |movie| movie[:duration].to_i }
+  puts "Movies by duration last 5:"
+  movies_list.sort_by(&:duration)
 end
 
 def comedies_by_date(movies_list)
+  puts "Comedies_by_date:"
   movies_list.select { |movie| movie if movie[:genre].include?("Comedy") }.
   sort_by { |movie| movie[:date_published] }
 end
 
 def all_movies_directors(movies_list)
+  puts "All_movies_directors:"
   movies_list.collect { |movie| movie[:director] }.uniq.
   sort_by { |director| director.split(" ").last }
 end
 
 def movies_produced_not_in_usa(movies_list)
+  puts "Movies_produced_not_in_usa:"
   movies_list.reject { |movie| movie[:county].include?("USA") }
 end
 
 
 def movies_group_by_directors(movies_list)
+  puts "Movies_group_by_directors:"
   movies_list.group_by { |movie| movie[:director] }.sort.
   collect { |director, movies| {director => movies.count} }
 end
 
 def actors_in_movies_count(movies_list)
+  puts "Actors_in_movies_count:"
   movies_list.collect { |movie| movie[:stars].strip.split(',') }.flatten.sort.
   reduce(Hash.new(0)) { |h, actor| h[actor] += 1; h }
 end
@@ -75,25 +81,14 @@ end
 
 
 def lesson_3(list)
-  puts "Movies by duration last 5:"
-    ap movies_by_duration(list).last(5)
-
-  puts "Comedies_by_date:"
-    ap comedies_by_date(list)
-
-  puts "All_movies_directors:"
-    ap all_movies_directors(list)
-
-  puts "Movies_produced_not_in_usa:"
-    ap movies_produced_not_in_usa(list)
+  ap movies_by_duration(list).last(5)
+  ap comedies_by_date(list)
+  ap all_movies_directors(list)
+  ap movies_produced_not_in_usa(list)
 
   puts "#BONUS"
-  puts "Movies_group_by_directors:"
-   ap movies_group_by_directors(list)
-
-  puts "Actors_in_movies_count:"
-   ap actors_in_movies_count(list)
-
+  ap movies_group_by_directors(list)
+  ap actors_in_movies_count(list)
 end
 
 movies_struct = struct_movies(movies_list_file)
